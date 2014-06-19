139319eba193aa0e6568a2945a71b73018e5dabc
Kate <kosuchenkok@mail.ru>
2014-06-19 10:48:30 +0300

    fix - easy image rollback

--- seocms/protected/extensions/easyimage/EasyImage.php
+++ seocms/protected/extensions/easyimage/EasyImage.php
@@ -236,8 +236,6 @@ class EasyImage extends CApplicationComponent
                     break;
                 case 'savePath':
                     break;
-                case 'save':
-                    break;
                 default:
                     throw new CException('Action "' . $key . '" is not found');
             }
@@ -254,15 +252,14 @@ class EasyImage extends CApplicationComponent
     public function thumbSrcOf($file, $params = array())
     {
         // Paths
-//        $hash = $this->getHashedName($file,$params,false);
-        $hash =$params['save'];
+        $hash = $this->getHashedName($file,$params,false);
         $cacheFileExt = isset($params['type']) ? $params['type'] : pathinfo($file, PATHINFO_EXTENSION);
         $cacheFileName = $hash . '.' . $cacheFileExt;
 
         if($params['savePath']){
-            $cachePath = $params['savePath'];
+            $cachePath = Yii::getpathOfAlias('webroot') . $params['savePath'];
             $webCacheFile = $cachePath.$cacheFileName;
-            $cacheFile = $params['savePath'] . $cacheFileName;
+            $cacheFile = Yii::getpathOfAlias('webroot').$params['savePath'] . $cacheFileName;
         } else {
             $cachePath = Yii::getpathOfAlias('webroot') . $this->cachePath . $hash{0};
             $webCacheFile = Yii::app()->baseUrl . $this->cachePath . $hash{0} . '/' . $cacheFileName;
@@ -274,10 +271,9 @@ class EasyImage extends CApplicationComponent
             return isset($params['savePath']) ? $params['savePath'].$cacheFileName : $webCacheFile;
         }
 
-        // Make cache dir //
-        if (!file_exists($cachePath)) {
-//            die($cachePath);
-            mkdir($cachePath, 0777);
+        // Make cache dir
+        if (!is_dir($cachePath)) {
+            mkdir($cachePath, 0755, true);
         }
 
         // Create and caching thumbnail use params
@@ -322,9 +318,11 @@ class EasyImage extends CApplicationComponent
      */
     public function thumbOf($file, $params = array(), $htmlOptions = array())
     {
-        return
-            $this->thumbSrcOf($file, $params);
-
+        return CHtml::image(
+            $this->thumbSrcOf($file, $params),
+            isset($htmlOptions['alt']) ? $htmlOptions['alt'] : '',
+            $htmlOptions
+        );
     }
 
     /**
---
+++ seocms/protected/modules/users/views/users/login.php
@@ -0,0 +1,47 @@
+<?php
+/* @var $this SiteController */
+/* @var $model LoginForm */
+/* @var $form CActiveForm  */
+
+//$this->pageTitle=Yii::app()->name . ' - Login';
+?>
+<section class="content">
+    <div class="form">
+        <?php $form=$this->beginWidget('CActiveForm', array(
+            'id'=>'login-form',
+            'enableClientValidation'=>true,
+            'clientOptions'=>array(
+                'validateOnSubmit'=>true,
+            ),
+            'htmlOptions'=>array(
+                'class'=>'login-form',
+            )
+        )); ?>
+
+        <div class="row">
+            <?php echo CHtml::label('E-mail', 'login'); ?>
+            <?php echo $form->textField($model,'username', array('class'=>'input-text', 'id'=>'login')); ?>
+            <?php echo $form->error($model,'username'); ?>
+        </div>
+
+        <div class="row">
+            <?php echo CHtml::label('Пароль', 'pass'); ?>
+            <?php echo $form->passwordField($model,'password', array('class'=>'input-text', 'id'=>'pass')); ?>
+            <?php echo $form->error($model,'password'); ?>
+        </div>
+
+<!--        <div class="row rememberMe">-->
+<!--            --><?php //echo $form->checkBox($model,'rememberMe'); ?>
+<!--            --><?php //echo $form->label($model,'rememberMe'); ?>
+<!--            --><?php //echo $form->error($model,'rememberMe'); ?>
+<!--        </div>-->
+
+        <div class="row buttons">
+            <?php echo CHtml::submitButton('Войти', array('class'=>'green-button')); ?>
+        </div>
+
+        <?php $this->endWidget(); ?>
+    </div>
+</section>
+
+<!-- form -->
